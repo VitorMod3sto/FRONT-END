f0e371a89b4de1a3043cf4efe62a528d4bcbb3f2
VitorMod3sto <vitorebrunomodesto@gmail.com>
2024-08-29 21:45:05 -0300

    pastas disney

---
+++ src/app/disney/cards/page.js
@@ -0,0 +1,13 @@
+'use client'
+
+import Pagina from "@/app/components/Pagina";
+import apiDisney from "@/services/apiDisney";
+
+export default function Page() {
+
+    return (
+        <Pagina titulo="Card Disney">
+          
+        </Pagina>
+    )
+}
