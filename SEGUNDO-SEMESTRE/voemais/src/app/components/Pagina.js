3b293586b41b51245109cb510ad67b6c5f4d0a48
VitorMod3sto <vitorebrunomodesto@gmail.com>
2024-09-23 21:53:04 -0300

    voemais

---
+++ SEGUNDO-SEMESTRE/voemais/src/app/components/Pagina.js
@@ -0,0 +1,40 @@
+import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
+
+export default function Pagina(props) {
+
+    return (
+        <>
+            <Navbar bg="dark" data-bs-theme="dark">
+                <Container>
+                    <Navbar.Brand href="/filmes">Filmes Revisão</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="/filmes">Filmes Revisão</Nav.Link>
+                        <Nav.Link href="/filmes">Filmes</Nav.Link>
+                        <Nav.Link href="/series">Series</Nav.Link>
+                        <Nav.Link href="/atores">Atores</Nav.Link>
+                        <NavDropdown title="Séries" id="basic-nav-dropdown"> 
+                            <NavDropdown.Item href="/series">
+                                Populares
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown title="Filmes" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="./filmes">
+                                Em cartaz
+                            </NavDropdown.Item>
+                            
+                            <NavDropdown.Divider />
+
+                        </NavDropdown>
+                    </Nav>
+                </Container>
+            </Navbar>
+
+            <div className="bg-secondary text-white text-center p-3">
+                <h1>{props.titulo}</h1>
+            </div>
+            <Container className="my-3">
+                {props.children}
+            </Container>
+        </>
+    )
+}
---
+++ SEGUNDO-SEMESTRE/voemais/src/app/empresas/create/page.js
@@ -0,0 +1,62 @@
+'use client'
+
+import Pagina from "@/app/components/Pagina";
+import { Formik } from "formik";
+import Link from "next/link";
+import { Button, Form } from "react-bootstrap";
+import { FaCheck } from "react-icons/fa";
+import { IoIosArrowRoundBack } from "react-icons/io";
+
+
+
+export default function Page() {
+
+function salvar(dados){
+    console.log(dados)
+}
+
+    return (
+        <Pagina titulo="Empresa">
+            
+            <Formik
+                initialValues={{ nome: '', logo: '' }}
+                onSubmit={values => salvar(values)}
+            >
+                {({
+                    values,
+                    handleChange,
+                    handleSubmit,
+                }) => (
+                    <Form>
+                        <Form.Group className="mb-3" controlId="nome">
+                            <Form.Label>Nome</Form.Label>
+                            <Form.Control 
+                            type="text"
+                             name="nome"
+                             value={values.nome}
+                             onChange={handleChange('nome')}
+                              />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="logo">
+                            <Form.Label>Logo</Form.Label>
+                            <Form.Control type="text"
+                             name="logo"
+                             value={values.logo}
+                             onChange={handleChange('logo')}
+                              />
+                        </Form.Group>
+
+                        <div className="text-center">
+                            <Button onClick={handleSubmit} variant="success">
+                                Salvar  <FaCheck />
+                            </Button>
+                            <Link href="/empresas" className="btn btn-danger ms-3" >
+                                <IoIosArrowRoundBack /> Voltar
+                            </Link>
+                        </div>
+                    </Form>
+                )}
+            </Formik>
+        </Pagina>
+    )
+}
